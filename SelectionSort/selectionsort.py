8381f3524476a541e97df7948c4f6464e5df8a13
Alexssander José de Oliveira de Castro <147998833+alexssander77@users.noreply.github.com>
2025-03-24 15:02:56 -0300

    Add files via upload

---
+++ SelectionSort/selectionsort.py
@@ -0,0 +1,22 @@
+import time
+
+def selection_sort(array):
+    n = len(array)
+    print("Vetor antes da ordenação:", array)
+    
+    inicio_tempo = time.time()
+    
+    for i in range(n):
+        menorP = i
+        for j in range(i + 1, n):
+            if array[j] < array[menorP]:
+                menorP = j
+        
+        
+        array[i], array[menorP] = array[menorP], array[i]
+        print(f"Iteração {i + 1}: {array} (Trocou {array[menorP]} com {array[i]})")
+       
+    
+    fim_tempo = time.time()
+    print("Vetor depois da ordenação:", array)
+    print(f"Tempo de execução: {fim_tempo - inicio_tempo:.6f} segundos")
---
+++ SelectionSort/selectionsortOtimizado.py
@@ -0,0 +1,23 @@
+import time
+
+def selection_sort_otimizado(array):
+    n = len(array)
+    print("Vetor antes da ordenação:", array)
+    
+    inicio_tempo = time.time()
+    
+    for i in range(n):
+        menorP = i
+        for j in range(i + 1, n):
+            if array[j] < array[menorP]:
+                menorP = j
+        
+        if i != menorP:
+            array[i], array[menorP] = array[menorP], array[i]
+            print(f"Iteração {i + 1}: {array} (Trocou {array[menorP]} com {array[i]})")
+        else:
+            print(f"Iteração {i + 1}: {array} (Sem troca)")
+    
+    fim_tempo = time.time()
+    print("Vetor depois da ordenação:", array)
+    print(f"Tempo de execução: {fim_tempo - inicio_tempo:.6f} segundos")
