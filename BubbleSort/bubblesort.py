bfb8d96e35aaaeba24a4ec10971d442eadda3743
Alexssander José de Oliveira de Castro <147998833+alexssander77@users.noreply.github.com>
2025-03-24 13:55:35 -0300

    Add files via upload

--- BubbleSort/bubblesort.py
+++ BubbleSort/bubblesort.py
@@ -1,7 +1,12 @@
+import time
+
 def bubble_sort(array):
     n = len(array)
+    start_time = time.time() 
     for i in range(n-1):
-        print("Iteração: ", i, array)
+        print("Iteração:", i, array)
         for j in range(n-1-i):
             if array[j] > array[j+1]:
                 array[j], array[j+1] = array[j+1], array[j]
+    end_time = time.time()  
+    print(f"Tempo de execução: {end_time - start_time:.6f} segundos")
--- BubbleSort/bubblesortOtimizado.py
+++ BubbleSort/bubblesortOtimizado.py
@@ -1,12 +1,18 @@
-def bubble_sort(array):
+import time
+
+def bubble_sort_otimizado(array):
     n = len(array)
+    start_time = time.time()  
     for i in range(n-1):
         troca_ocorreu = False  
-        print("Iteração:", i, array)
+        print("Iteração:", i, array)
         for j in range(n-1-i):
             if array[j] > array[j+1]:
                 array[j], array[j+1] = array[j+1], array[j]
                 troca_ocorreu = True  
 
         if not troca_ocorreu:  
-            break
+            break  
+    
+    end_time = time.time()  
+    print(f"Tempo de execução: {end_time - start_time:.6f} segundos")
